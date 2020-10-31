b75097f0fbf47d38ead699dc3b43b5db062d9967
Kirill <kirill.kuzvietsov@mail.ru>
2020-10-31 13:59:29 +0300

    Scripted auto-commit on change (2020-10-31 13:59:29) by gitwatch.sh

--- Hadoop_lab_2/src/main/java/org/bmstu/IU9/hadoop_labs/lab_2/FlightReduce.java
+++ Hadoop_lab_2/src/main/java/org/bmstu/IU9/hadoop_labs/lab_2/FlightReduce.java
@@ -1,6 +1,14 @@
 package org.bmstu.IU9.hadoop_labs.lab_2;
 
-public class FlightReduce {
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+import java.util.Iterator;
+
+public class FlightReduce extends Reducer<Text, IntWritable, Text, LongWritable> {
     @Override
     protected void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
 
