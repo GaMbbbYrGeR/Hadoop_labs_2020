b65ffaf390e785400622c4548f3e702459f19c3e
Kirill <kirill.kuzvietsov@mail.ru>
2020-10-31 13:29:04 +0300

    Scripted auto-commit on change (2020-10-31 13:29:04) by gitwatch.sh

--- Hadoop_lab_2/src/main/java/org/bmstu/IU9/hadoop_labs/lab_2/FlightsMap.java
+++ Hadoop_lab_2/src/main/java/org/bmstu/IU9/hadoop_labs/lab_2/FlightsMap.java
@@ -1,4 +1,30 @@
 package org.bmstu.IU9.hadoop_labs.lab_2;
 
-public class FlightsMap {
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+public class FlightsMap extends Mapper<LongWritable, Text, FlightWritableComparable, Text> {
+    @Override
+    protected void map(LongWritable key, Text value, Mapper.Context context) throws IOException, InterruptedException {
+
+        final String SPLITTER = ",";
+        final String COMPARE = "\"YEAR\"";
+        final int FLAG_FLIGHT = 1;
+        final float NO_CANCELED = 0;
+
+        String[] str = value.toString().split(SPLITTER);
+
+        if(!str[0].equals(COMPARE) && !str[18].isEmpty()) {
+            if(Float.parseFloat(str[18]) != NO_CANCELED) {
+                FlightWritableComparable flight = new FlightWritableComparable(FLAG_FLIGHT, Integer.parseInt(str[14]));
+
+                Text name = new Text(str[18]);
+
+                context.write(flight, name);
+            }
+        }
+    }
 }
