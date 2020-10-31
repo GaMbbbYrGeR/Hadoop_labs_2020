9f94630617f869e8337022063d9668440eba4a85
Kirill <kirill.kuzvietsov@mail.ru>
2020-10-31 13:18:15 +0300

    Scripted auto-commit on change (2020-10-31 13:18:15) by gitwatch.sh

--- Hadoop_lab_2/src/main/java/org/bmstu/IU9/hadoop_labs/lab_2/AirportsMap.java
+++ Hadoop_lab_2/src/main/java/org/bmstu/IU9/hadoop_labs/lab_2/AirportsMap.java
@@ -9,9 +9,22 @@ import java.io.IOException;
 public class AirportsMap extends Mapper<LongWritable, Text, FlightWritableComparable, Text> {
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-        String[] str = value.toString().split(",");
 
-        
+        final String SPLITTER = ",(?! )";
+        final String REPLACE = "\"";
+        final String REPLACEMENT = "";
+        final String COMPARE = "Code";
+        final int FLAG_AIRPORT = 0;
+
+        String[] str = value.toString().replaceAll(REPLACE, REPLACEMENT).split(SPLITTER);
+
+        if(!str[0].equals(COMPARE)) {
+            FlightWritableComparable flight = new FlightWritableComparable(FLAG_AIRPORT, Integer.parseInt(str[0]));
+
+            Text name = new Text(str[1]);
+
+            context.write(flight, name);
+        }
     }
 
 }
