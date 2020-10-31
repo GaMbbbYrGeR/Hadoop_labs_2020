fbc5f076043b1f6b39c8a2dc0d20379fd2bc0d97
Kirill <kirill.kuzvietsov@mail.ru>
2020-10-31 13:40:25 +0300

    Scripted auto-commit on change (2020-10-31 13:40:25) by gitwatch.sh

--- Hadoop_lab_2/src/main/java/org/bmstu/IU9/hadoop_labs/lab_2/FlightGroupingComparator.java
+++ Hadoop_lab_2/src/main/java/org/bmstu/IU9/hadoop_labs/lab_2/FlightGroupingComparator.java
@@ -7,4 +7,17 @@ public class FlightGroupingComparator implements RawComparator {
     public int compare(FlightWritableComparable a, FlightWritableComparable b) {
         return a.compareTo(b);
     }
+
+    public class NaturalKeyGroupingComparator extends WritableComparator {
+        protected NaturalKeyGroupingComparator() {
+            super(StockKey.class, true);
+        }
+        @SuppressWarnings("rawtypes")
+        @Override
+        public int compare(WritableComparable w1, WritableComparable w2) {
+            StockKey k1 = (StockKey)w1;
+            StockKey k2 = (StockKey)w2;
+
+            return k1.getSymbol().compareTo(k2.getSymbol());
+        }
 }
