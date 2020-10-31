1661f1cd8f47f35eba2c37945624d9af8d7034e7
Kirill <kirill.kuzvietsov@mail.ru>
2020-10-31 13:36:43 +0300

    Scripted auto-commit on change (2020-10-31 13:36:43) by gitwatch.sh

--- Hadoop_lab_2/src/main/java/org/bmstu/IU9/hadoop_labs/lab_2/FlightPartition.java
+++ Hadoop_lab_2/src/main/java/org/bmstu/IU9/hadoop_labs/lab_2/FlightPartition.java
@@ -4,4 +4,7 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Partitioner;
 
 public class FlightPartition extends Partitioner<FlightWritableComparable, Text> {
+    public int getPartition(FlightWritableComparable key, Text value, int numReduceTasks) {
+        return key.get_dectairip() % numReduceTasks;
+    }
 }
