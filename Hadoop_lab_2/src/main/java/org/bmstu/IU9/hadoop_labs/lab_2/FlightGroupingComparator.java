42eb9766a8df1e21e1d120a11018c2a171efdf5b
Kirill <kirill.kuzvietsov@mail.ru>
2020-10-31 13:39:48 +0300

    Scripted auto-commit on change (2020-10-31 13:39:48) by gitwatch.sh

--- Hadoop_lab_2/src/main/java/org/bmstu/IU9/hadoop_labs/lab_2/FlightGroupingComparator.java
+++ Hadoop_lab_2/src/main/java/org/bmstu/IU9/hadoop_labs/lab_2/FlightGroupingComparator.java
@@ -3,4 +3,8 @@ package org.bmstu.IU9.hadoop_labs.lab_2;
 import org.apache.hadoop.io.RawComparator;
 
 public class FlightGroupingComparator implements RawComparator {
+
+    public int compare(FlightWritableComparable a, FlightWritableComparable b) {
+        return a.compareTo(b);
+    }
 }
