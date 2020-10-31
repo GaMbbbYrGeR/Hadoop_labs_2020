39311cbb4cfe18f1c9181312ca4b6694f9bd2e47
Kirill <kirill.kuzvietsov@mail.ru>
2020-10-31 13:05:32 +0300

    Scripted auto-commit on change (2020-10-31 13:05:32) by gitwatch.sh

--- Hadoop_lab_2/src/main/java/org/bmstu/IU9/hadoop_labs/lab_2/AirportsMap.java
+++ Hadoop_lab_2/src/main/java/org/bmstu/IU9/hadoop_labs/lab_2/AirportsMap.java
@@ -1,5 +1,22 @@
 package org.bmstu.IU9.hadoop_labs.lab_2;
 
-public class AirportsMap {
-    
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+public class AirportsMap extends Mapper<LongWritable, Text, FlightWritableComparable, Text> {
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        String[] str = value.toString().split(",");
+
+        if(Float.parseFloat(str[19]) != (float)1 && !str[0].equals("YEAR")) {
+            if(Float.parseFloat(str[18]) != (float)0) {
+                FlightWritableComparable flight = new FlightWritableComparable();
+                flight.set_flag(1);
+            }
+        }
+    }
+
 }
