7f03dc281c881fa719959544a6f997cef3a2abb8
Kirill <kirill.kuzvietsov@mail.ru>
2020-10-31 13:58:53 +0300

    Scripted auto-commit on change (2020-10-31 13:58:53) by gitwatch.sh

--- Hadoop_lab_2/src/main/java/org/bmstu/IU9/hadoop_labs/lab_2/FlightReduce.java
+++ Hadoop_lab_2/src/main/java/org/bmstu/IU9/hadoop_labs/lab_2/FlightReduce.java
@@ -1,4 +1,15 @@
 package org.bmstu.IU9.hadoop_labs.lab_2;
 
 public class FlightReduce {
+    @Override
+    protected void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+
+        long count = 0;
+        Iterator iter =  values.iterator();
+        while (iter.hasNext()) {
+            iter.next();
+            count++;
+        }
+        context.write ( key, new LongWritable ( count));
+    }
 }
